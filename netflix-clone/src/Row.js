e3934d164ace173afc3bb4628f5e9671cad8aeff
Roemer Naber <200611@student.glu.nl>
2022-04-01 12:39:57 +0200

    films request on app met css scrollbar

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios'
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -26,11 +27,12 @@ function Row({ title, fetchUrl }) {
             <h2>{title}</h2>
 
             {/* container -> posters */}
-            <div className="row_posters">
+            <div className="row__posters">
                 {/* row_poster */}
                 {movies.map(movie => (
                     <img
-                        className="row_poster"
+                        key={movie.id} //het scrollen gaat iets sneller
+                        className="row__poster"
                         src={`${base_url}${movie.poster_path}`} alt={movie.name} />
                 ))}
             </div>
