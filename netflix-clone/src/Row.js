3feb55e6d9e03a26673c200c40d04ff30bfb5909
Roemer Naber <200611@student.glu.nl>
2022-04-07 23:32:58 +0200

    laatste commit voor webpack 4 configuratie naar webpack 5

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import axios from './axios'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 import "./Row.css";
-// import YouTube from "react-youtube";
-// import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
-    // const [trailerUrl, setTrailerUrl] = useState("");
+    const [trailerUrl, setTrailerUrl] = useState("");
+
 
     //snippet of code die rund op een specifieke cindition/varaible
     useEffect(() => {
@@ -23,29 +24,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
-    // const opts = {
-    //     height: "390",
-    //     width: "100%",
-    //     playerVars: {
-    //         // https://developers.google.com/youtube/player_parameters
-    //         autoplay: 1,
-    //     },  
-    // };
+    // console.table(movies);
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
 
-    // const handleClick = (movie) => {
-    //     if(trailerUrl) {
-    //         setTrailerUrl('');
-    //     } else {
-    //         movieTrailer(movie?.name || "")
-    //         .then(url => {
-    //             const urlParams = new URLSearchParams (new URL(url).search);
-    //             setTrailerUrl(urlParams.get("v"));
-    //         })
-    //         .catch((error) => console.log(error));
-    //     }
-    // }
-
-    console.table(movies);
+      const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+          movieTrailer(movie?.name || "")
+          .then((url) => {
+            // https://www.youtube.com/watch?v=XtMThy8QKqU we gaan de code wat achter de 'v' staat want dat is het ID data
+            const urlParams = new URLSearchParams (new URL(url).search);
+            setTrailerUrl(urlParams.get("v"));
+          })
+          .catch((error) => console.log(error));
+        }
+      };
 
     return (
         <div className="row">
@@ -57,14 +58,16 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {movies.map((movie) => (
                     <img
                         key={movie.id} //het scrollen gaat iets sneller
-                        // onClick={() => handleClick(movie)}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${base_url}${
                             isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name} />
                 ))}
             </div>
-            {/* {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />} */}
+            {/* dit was een test */}
+            {/* <YouTube videoId="XtMThy8QKqU" opts={opts} /> */}
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
